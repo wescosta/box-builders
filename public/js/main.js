882f5a4c16cedadfe2bc75e522f85c397ba6eb10
Wesley Anemam <wesleylira@gmail.com>
2016-06-17 19:17:15 -0400

    Removed console logs

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,8 @@ import Ambient from './ambient.js'
 
 const io = Socket(location.host)
 
-io.on('hi', message => console.log(message))
-
 Ambient
   .init()
-  .on('add', box => {
-    console.log('Ship the box through the socket >>', box)
-    io.emit('add', box)
-})
+  .on('add', box => io.emit('add', box))
 
-io.on('add', box => {
-    console.log('Yo! Got a box from the socket', box)
-  Ambient.add(box)})
+io.on('add', box => Ambient.add(box))
